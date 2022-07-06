136390bb454665d0432d29a2f92c92a6b95f3e14
boralevi <barlavigabriel@gmail.com>
2022-07-06 22:29:39 +0200

    réécriture du script de changement de couleur pour le titre + script de changement de couleur de fond sur clic du boutonback to top avec ajout de d'une fonction de changement d'image sur la main_image de la page d'accueil.

--- static/myJS/script.js
+++ static/myJS/script.js
@@ -4,8 +4,13 @@ if (firstscript) {
 } else {
   alert("J'ai créé une boucle if et ici nous passons bien dans le else");
 }
-
-
+function random(number) {
+  return Math.floor(Math.random()*(number+1));
+}
+function bgChange(){
+var rndCol = 'rgba(' + random(255) + ',' + random(255) + ',' + random(255) + ',' + .2 + ')';
+document.body.style.backgroundColor = rndCol;
+};
 function multiply(num1,num2) {
   let result = num1 * num2;
   return result;
@@ -24,23 +29,30 @@ myH1.addEventListener('click', function() {alert('prout premier h1 tag')});
 
 
 // exemple encore à tester
-let myImage = document.querySelector('img');
+myImage = document.getElementById('main_image');
 
-myImage.addEventListener('click', function() {
+myImage.addEventListener('mouseover', function() {
     let mySrc = myImage.getAttribute('src');
-    if (mySrc === 'images/firefox-icon.png') {
-      myImage.setAttribute('src', 'images/firefox2.png');
-    } else {
-      myImage.setAttribute('src', 'images/firefox-icon.png');
+    console.log(mySrc)
+    if (mySrc === '../static/img/HSV.jpg') {
+      myImage.setAttribute('src', '../static/img/HSV-matrix.jpg');
+    } else {console.log('gros prout');
+      myImage.setAttribute('src', '../static/img/HSV.jpg');
     }
 });
 
-let balise = false;
+let balise = true;
 function ChangerCouleur(Identifiant){
-    document.getElementById(Identifiant).style.color="red";
+var rndCol = 'rgba(' + random(255) + ',' + random(255) + ',' + random(255) + ',' + 1 + ')'
+    document.getElementById(Identifiant).style.color= rndCol;
 balise = true;
 console.log(balise);
 };
+
+myTitle = document.getElementById("mytitle1")
+myTitle.addEventListener('mouseover',function(){if (balise === true){ChangerCouleur('mytitle1');
+console.log(balise)
+}});
 
 myH1.addEventListener('clik', function(){
 if (balise === true){ChangerCouleur(Identifiant);
@@ -60,38 +72,39 @@ console.log(document.getElementById("footer"));
 
 var divbtt = document.getElementById("divbtt"),
     btnbtt = document.getElementById("btnbtt"),
-    effect = btnbtt.getAttribute('aria-pressed'),
     body = document.body,
     scrollMax = document.scrollingElement.scrollTopMax,
     docElem = document.documentElement,
     offset = 100,
     isFirefox = navigator.userAgent.toLowerCase().indexOf("firefox")> -1,
     scrollPos, docHeight;
-console.log(effect);
+    console.log(effect);
     docHeight = Math.max(body.scrollHeight, body.offsetHeight,
      docElem.clientHeight, docElem.scrollHeight, docElem.offsetHeight);
+
+
     if (docHeight != 'undefined'){
       offset = docHeight*0.95;
     }
-
+    if (divbtt){//pour éviter des erreurs "uncaught" dans la console quand l'utilisateur n'est pas connecté et que
+    //le bouton n'est pas disponible pour cette fonction.
     window.addEventListener("scroll", function(event) {
     scrollPos = body.scrollTop || docElem.scrollTop;
-     console.log(docHeight),
-     console.log(offset),
-     console.log(document.scrollingElement.scrollTopMax),
-     console.log(scrollPos);
      divbtt.className = (scrollPos > scrollMax*0.95) ? "visible" : "invisible"
-    });
+    });}
 
-    btnbtt.addEventListener("click", function(event){
+if (btnbtt){//cf.com plus haut, idem
+    var effect = btnbtt.getAttribute('aria-pressed');
+    btnbtt.addEventListener("click", function(event){bgChange();
     event.preventDefault();
     if (isFirefox){
     docElem.scrollTop = 0;
-    } else { body.scrollTop = 0;}
-    console.log(btnbtt.getAttribute('aria-pressed'))
     }
-    );
+    else { body.scrollTop = 0;}
+    btnbtt.setAttribute('class', 'btn position-fixed bottom-0 end-50');
+    console.log(effect);
+    });};
 console.log(effect);
-
+document.querySelector('p').click()
 
 
