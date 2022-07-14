cdf0fba4b3828129e64581a3e54dcc0cdcbb67a0
boralevi <barlavigabriel@gmail.com>
2022-07-14 17:35:38 +0200

    commit pour github. essai sur le bouton back to top.inclus dans une variable de block {% block footer %} pour qu'il s'insère dans le footer de la page main.html

--- static/myJS/articles_anim.js
+++ static/myJS/articles_anim.js
@@ -1,25 +1,24 @@
-const elements = document.querySelectorAll('a');
+// const elements = document.querySelectorAll('a');
 
-
-for(i =0; i< elements.length;i++){console.log(elements.NodeList)}
-gsap.to(child,
-    {
-    duration:1,
-    scrollTrigger:
-      {
-        trigger: child,
-        container: "#section2",
-        start: "top 90%",
-        end: "top 50%",
-        markers: {fontSize: "2rem"},
-        toggleClass: "invisible",
-        //toggleActions:"none none none none",
-        //            onenter onLeave  onEnterback   onLeaveBack
-        onToggle: self =>  console.log(child.className),
-        //pinSpacing: false,
-        //pin: true          
+// for(i =0; i< elements.length;i++){console.log(elements.NodeList)}
+// gsap.to(child,
+//     {
+//     duration:1,
+//     scrollTrigger:
+//       {
+//         trigger: child,
+//         container: "#section2",
+//         start: "top 90%",
+//         end: "top 50%",
+//         markers: {fontSize: "2rem"},
+//         toggleClass: "invisible",
+//         //toggleActions:"none none none none",
+//         //            onenter onLeave  onEnterback   onLeaveBack
+//         onToggle: self =>  console.log(child.className),
+//         //pinSpacing: false,
+//         //pin: true          
         
-      }
-    }
-    )
-;
+//       }
+//     }
+//     )
+// ;
