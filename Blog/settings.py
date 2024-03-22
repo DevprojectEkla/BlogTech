641a6370568531b634b6422b6f50a3182f2de46b
ekla <barlavigabriel@gmail.com>
2024-03-22 19:32:54 +0100

    --amend

--- Blog/settings.py
+++ Blog/settings.py
@@ -27,6 +27,7 @@ DATABASES = {"default" : dj_database_url.parse(DATABASE_URL),
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY") 
+print(f"secret_key: {SECRET_KEY}")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
