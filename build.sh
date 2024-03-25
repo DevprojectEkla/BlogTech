6c19a93c69aec8a157c445006c8ba6ecb0a12a2f
Ekla <108277748+DevprojectEkla@users.noreply.github.com>
2024-03-25 10:18:45 +0100

    Update README.md

--- build.sh
+++ build.sh
@@ -9,9 +9,17 @@
 ######################################################################
 
 set -o errexit
+
+handle_error(){
+    echo "an error occured. Continuing with the build..."
+}
+
+trap 'handle_error' ERR
+
+echo Building...
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
-python manage.py createsuperuser --noinput
+python manage.py createsuperuser --noinput | true
 
